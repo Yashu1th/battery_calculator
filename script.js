0628cd93447084fbc855cb3bb2655c7edd2a57a7
yaswanth narikamalli <yaswanthnarikamali@gmail.com>
2023-06-18 16:11:17 +0530

    optimised

--- script.js
+++ script.js
@@ -10,23 +10,30 @@ function calculate(){
     var drain=document.getElementById("drain").value;
     document.getElementById("drain-val").textContent=drain+" %";
     var secs=86400;
-    var mins=1440;
+    
     var active =document.getElementById('active-time').value;
     if(document.getElementById('input1').checked){
-        oneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active);
-        pOneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active);
+        var sleep=60-active;
+        var mins=1440;
+        oneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active,sleep);
+        pOneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active,sleep);
     }
     else if (document.getElementById('input2').checked){
-        fiveMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active);
-        pFiveMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active);
+        var sleep=300-active;
+        var mins=1440;
+        var mins=mins/5;
+        oneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active,sleep);
+        pOneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active,sleep);
     }
     else if(document.getElementById('input3').checked){
-
-        oneHour(mins,pow_consumption,ds_pow_con,battery_capacity,active);
-        pOneHour(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active);
+        var sleep=3600-active;
+        var mins=1440;
+        var mins=mins/60;
+        oneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active,sleep);
+        pOneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active,sleep);
     }
     else if(document.getElementById('input4').checked){
-
+        var mins=1440;
         whNwh(mins,pow_consumption,ds_pow_con,battery_capacity,work_hrs,active);
         pWhNwh(mins,pow_consumption,ds_pow_con,battery_capacity,work_hrs,drain,active);
     }
@@ -35,23 +42,9 @@ function calculate(){
     }
 }
 
-function oneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active){
-    var sleep=60-active
+function oneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active,sleep){
+    
     var total=(((mins*active)/60)*pow_consumption+((mins*sleep)/60)*ds_pow_con)/60;
-    var output=Math.floor(battery_capacity/total);
-    var result=document.getElementById("result");
-    result.textContent=`${output} days`;
-}
-function fiveMinute(mins,pow_consumption,ds_pow_con,battery_capacity,active){
-    var sleep=300-active;
-    var total=((((mins/5)*active)/60)*pow_consumption+(((mins/5)*sleep)/60)*ds_pow_con)/60;
-    var output=Math.floor(battery_capacity/total);
-    var result=document.getElementById("result");
-    result.textContent=`${output} days`;
-}
-function oneHour(mins,pow_consumption,ds_pow_con,battery_capacity,active){
-    var sleep=3600-active
-    var total=((((mins/60)*active)/60)*pow_consumption+(((mins/60)*sleep)/60)*ds_pow_con)/60;
     var output=Math.floor(battery_capacity/total);
     var result=document.getElementById("result");
     result.textContent=`${output} days`;
@@ -70,23 +63,8 @@ function whNwh(mins,pow_consumption,ds_pow_con,battery_capacity,work_hrs,active)
     var result4=document.getElementById("result");
     result4.textContent=`${output4} days`;
 }
-function pOneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active){
-    var sleep=60-active;
+function pOneMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active,sleep){
     var total=(((mins*active)/60)*pow_consumption+((mins*sleep)/60)*ds_pow_con)/60;
-    var output=Math.floor((battery_capacity-(drain*0.01*battery_capacity))/total);
-    var result=document.getElementById("result1");
-    result.textContent=`${output} days`;
-}
-function pFiveMinute(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active){
-    var sleep=300-active;
-    var total=((((mins/5)*active)/60)*pow_consumption+(((mins/5)*sleep)/60)*ds_pow_con)/60;
-    var output=Math.floor((battery_capacity-(drain*0.01*battery_capacity))/total);
-    var result=document.getElementById("result1");
-    result.textContent=`${output} days`;
-}
-function pOneHour(mins,pow_consumption,ds_pow_con,battery_capacity,drain,active){
-    var sleep=3600-active;
-    var total=((((mins/60)*active)/60)*pow_consumption+(((mins/60)*sleep)/60)*ds_pow_con)/60;
     var output=Math.floor((battery_capacity-(drain*0.01*battery_capacity))/total);
     var result=document.getElementById("result1");
     result.textContent=`${output} days`;
